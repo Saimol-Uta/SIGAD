6024af243c8ac863718c702da913a85c07372bd3
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-06-03 12:34:54 -0500

    se creo un diseño de puerbas

---
+++ SIGAD.BlazorApp/Models/RangoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SIGAD.BlazorApp.Models
+{
+    public class RangoDto
+    {
+        public Guid Id { get; set; }
+        public string? Nombre { get; set; } // Usar string? para C# moderno con nulabilidad habilitada
+        public string? Descripcion { get; set; }
+    }
+}
