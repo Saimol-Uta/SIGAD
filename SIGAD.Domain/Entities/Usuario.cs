4929ba2f36bb9fa1cd10a363c5ae79e973ef0146
carol104 <carol.28427@gmail.com>
2025-06-04 09:43:12 -0500

    se añadió el contenido inicial a las clases del dominio (enums y entidades)

--- SIGAD.Domain/Entities/Usuario.cs
+++ SIGAD.Domain/Entities/Usuario.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SIGAD.Domain.Enums;
 
 namespace SIGAD.Domain.Entities
 {
-    internal class Usuario
+    public class Usuario
     {
+        public string Correo { get; set; } = null!;
+        public string PrimerNombre { get; set; } = null!;
+            public string SegundoNombre { get; set; } = null!;
+            public string PrimerApellido { get; set; } = null!;
+            public string SegundoApellido { get; set; } = null!;
+            public string Clave { get; set; } = null!;
+            public RolesUsuario Rol { get; set; }
+            public int? NivelTituloDocente { get; set; }
+
+            // Relaciones
+            public ICollection<Solicitud>? SolicitudesHechas { get; set; }
+            public ICollection<Documento>? Documentos { get; set; }
+        }
+
     }
 }
