7cf2c0f269c72fad79274e223f4ad661527cc470
carol104 <carol.28427@gmail.com>
2025-06-10 10:14:41 -0500

    cambios

---
+++ SIGAD.Domain/Entities/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGAD.Domain.Entities
+{
+    public class Usuario
+    {
+        public string Correo { get; set; }
+        public string Nombre1 { get; set; }
+    }
+}
