a87a9589c6a2301b8b696ca831019c1fdbb97dac
carol104 <carol.28427@gmail.com>
2025-06-11 10:34:23 -0500

    se creó el Dashboard para el Admin que redirige a la página de las solicitudes

--- SIGAD.BlazorApp/Models/VerSolicitudDTO.cs
+++ SIGAD.BlazorApp/Models/VerSolicitudDTO.cs
@@ -2,5 +2,11 @@
 {
     public class VerSolicitudDTO
     {
-    }
+            public Guid Id { get; set; }
+            public string NombreDocente { get; set; }
+            public string RangoActual { get; set; }
+            public string RangoSolicitado { get; set; }
+            public string Estado { get; set; }
+            public DateTime FechaCreacion { get; set; }
+        }
 }
