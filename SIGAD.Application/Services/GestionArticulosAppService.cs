991c32e3e5f20c628c2c755b23dfc591d27f2f9b
Manuel <mramirez1561@uta.edu.ec>
2025-06-11 16:06:09 -0500

    Plantilla inicial

--- SIGAD.Application/Services/GestionArticulosAppService.cs
+++ SIGAD.Application/Services/GestionArticulosAppService.cs
@@ -1,4 +1,6 @@
 ﻿using SIGAD.Application.DTOs;
+using SIGAD.Domain.Entities;
+using SIGAD.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +11,12 @@ namespace SIGAD.Application.Services
 {
     public class GestionArticulosAppService
     {
-        public GestionArticulosAppService(/* dependencias como IArticuloRepository */)
+        private readonly IArticuloRepository _articuloRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public GestionArticulosAppService(IArticuloRepository articuloRepository, IUnitOfWork unitOfWork)
         {
+            _articuloRepository = articuloRepository;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task CrearArticuloAsync(CrearArticuloDto dto, string docenteCedula)
