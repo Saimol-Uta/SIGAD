f927146647b078ab399e47de211c728e0a2a23c8
carol104 <carol.28427@gmail.com>
2025-06-12 08:16:25 -0500

    se crearon los diseños para el proceso de documentación

---
+++ SIGAD.BlazorApp/Models/AsensoEstadoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SIGAD.BlazorApp.Models
+{
+    public class AscensoEstadoDto
+    {
+        public ActualizarRangoDto Requisitos { get; set; }
+
+        public string EstadoArticulos { get; set; } = "Sin Revisar";
+        public string EstadoExperiencia { get; set; } = "Sin Revisar";
+        public string EstadoCursos { get; set; } = "Sin Revisar";
+        public string EstadoInvestigacion { get; set; } = "Sin Revisar";
+        public string EstadoEvaluaciones { get; set; } = "Sin Revisar";
+    }
+
+}
