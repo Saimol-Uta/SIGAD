799fddd3d16bc8fc9e8a0e2c851b4776cd417c73
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-06-11 23:31:51 -0500

    se implemento correctamente el login en el diseño

---
+++ SIGAD.BlazorApp/Models/LoginResponseDto.cs
@@ -0,0 +1,28 @@
+﻿namespace SIGAD.BlazorApp.Models
+{
+    // Necesitamos un enum para el Rol, igual que en el backend
+    public enum Rol
+    {
+        DOCENTE = 1,
+        ADMINISTRADOR = 2
+    }
+
+    public class LoginResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public Rol Rol { get; set; }
+        public DocenteInfoDto DocenteInfo { get; set; } = new();
+        public DateTime ExpiracionToken { get; set; }
+    }
+
+    public class DocenteInfoDto
+    {
+        public string Cedula { get; set; } = string.Empty;
+        public string Nombre1 { get; set; } = string.Empty;
+        public string? Nombre2 { get; set; }
+        public string Apellido1 { get; set; } = string.Empty;
+        public string Apellido2 { get; set; } = string.Empty;
+        public string NombreCompleto => $"{Nombre1} {Nombre2} {Apellido1} {Apellido2}".Replace("  ", " ").Trim();
+    }
+}
