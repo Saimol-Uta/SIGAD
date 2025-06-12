2be1175ad581b6102bd6ca1e7cece74dac19af04
Manuel <mramirez1561@uta.edu.ec>
2025-06-11 22:56:47 -0500

    Se crearon los AppService para Gestion de Investigaciones y Articulos

--- SIGAD.Infrastructure/Migrations/20250611222802_InitialSchema.Designer.cs
+++ SIGAD.Infrastructure/Migrations/20250611222802_InitialSchema.Designer.cs
@@ -12,7 +12,7 @@ using SIGAD.Infrastructure.Persistence;
 namespace SIGAD.Infrastructure.Migrations
 {
     [DbContext(typeof(SigadDbContext))]
-    [Migration("20250611162112_InitialSchema")]
+    [Migration("20250611222802_InitialSchema")]
     partial class InitialSchema
     {
         /// <inheritdoc />
