aed447bcd63ed8ef7dd92f541761b14962642376
carol104 <carol.28427@gmail.com>
2025-06-30 21:22:47 -0500

    Se colocó el header en el historial de solicitudes

--- SIGAD.Infrastructure/Migrations/20250701022150_InitialSchema.Designer.cs
+++ SIGAD.Infrastructure/Migrations/20250701022150_InitialSchema.Designer.cs
@@ -12,7 +12,7 @@ using SIGAD.Infrastructure.Persistence;
 namespace SIGAD.Infrastructure.Migrations
 {
     [DbContext(typeof(SigadDbContext))]
-    [Migration("20250701013611_InitialSchema")]
+    [Migration("20250701022150_InitialSchema")]
     partial class InitialSchema
     {
         /// <inheritdoc />
