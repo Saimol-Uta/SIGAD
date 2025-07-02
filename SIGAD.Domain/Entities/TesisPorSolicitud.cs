bc939be508e4b2a25c616c6e3002cc08e00f18a3
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-07-02 15:04:58 -0500

    Correcion Entities

---
+++ SIGAD.Domain/Entities/TesisPorSolicitud.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGAD.Domain.Entities
+{
+    /// <summary>
+    /// Entidad de relación entre SolicitudAscenso y TesisDirigida
+    /// para el requerimiento de tesis dirigidas en el proceso de ascenso
+    /// </summary>
+    public class TesisPorSolicitud
+    {
+        public Guid SolicitudId { get; set; } // Parte 1 de la Clave Primaria Compuesta
+        public int TesisId { get; set; } // Parte 2 de la Clave Primaria Compuesta
+
+        // Propiedades de navegación
+        public virtual SolicitudAscenso SolicitudAscenso { get; set; } = null!;
+        public virtual TesisDirigida TesisDirigida { get; set; } = null!;
+    }
+}
