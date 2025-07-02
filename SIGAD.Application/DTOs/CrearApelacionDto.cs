536345fa98b5979d134132f23947637d22c9c9f2
carol104 <carol.28427@gmail.com>
2025-07-02 15:32:09 -0500

    Se creó una nueva entidad para la apelación

---
+++ SIGAD.Application/DTOs/CrearApelacionDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SIGAD.Application.DTOs
+{
+    public class CrearApelacionDto
+    {
+        [Required]
+        public Guid SolicitudId { get; set; }
+
+        [Required(ErrorMessage = "El motivo de apelación es obligatorio.")]
+        public string Motivo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Debe proporcionar el enlace al documento.")]
+        [Url(ErrorMessage = "Debe ser un enlace válido.")]
+        public string DocumentoUrl { get; set; } = string.Empty;
+    }
+
+
+}
