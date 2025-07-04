2518219e4365680491d70e8f58cd5edb647ad22f
carol104 <carol.28427@gmail.com>
2025-07-04 11:37:13 -0500

    arreglos del idioma

--- SIGAD.Infrastructure/Migrations/20250704162005_InitialSchema.Designer.cs
+++ SIGAD.Infrastructure/Migrations/20250704162005_InitialSchema.Designer.cs
@@ -12,7 +12,7 @@ using SIGAD.Infrastructure.Persistence;
 namespace SIGAD.Infrastructure.Migrations
 {
     [DbContext(typeof(SigadDbContext))]
-    [Migration("20250704154827_InitialSchema")]
+    [Migration("20250704162005_InitialSchema")]
     partial class InitialSchema
     {
         /// <inheritdoc />
